6433603baee839ff4002f4427d3f02cb8a6b5ba3
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-01-02 17:28:49 +0100

    Initial commit

---
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/entities/ProjectSkeletonEntity.kt
@@ -0,0 +1,10 @@
+package com.pseandroid2.dailydata.model.database.entities
+
+data class ProjectSkeletonEntity(
+    val name: String,
+    val description: String,
+    val wallpaper: String,
+    val layout: String
+) {
+
+}
