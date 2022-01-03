6f57db0fba784399a5b7daf642bac3362af7eeb3
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-01-03 18:28:59 +0100

    Add Room Annotations

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/entities/ProjectEntity.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/entities/ProjectEntity.kt
@@ -1,10 +1,14 @@
 package com.pseandroid2.dailydata.model.database.entities
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.pseandroid2.dailydata.model.User
 
+@Entity(tableName = "project")
 data class ProjectEntity(
-    val id: Int,
-    val skeleton: ProjectSkeletonEntity,
+    @PrimaryKey val id: Int,
+    @Embedded val skeleton: ProjectSkeletonEntity,
     val admin: User,
     val onlineId: Long
 ) {
