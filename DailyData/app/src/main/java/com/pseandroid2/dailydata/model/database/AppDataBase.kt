2b4f890ff2bc777716bdaca53ed60cd83ec1a426
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-01-04 12:50:46 +0100

    Add TypeConverters and DAO getters

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/AppDataBase.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/AppDataBase.kt
@@ -2,9 +2,29 @@ package com.pseandroid2.dailydata.model.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.pseandroid2.dailydata.model.database.daos.GraphCDManager
+import com.pseandroid2.dailydata.model.database.daos.ProjectCDManager
+import com.pseandroid2.dailydata.model.database.daos.ProjectDataDAO
 import com.pseandroid2.dailydata.model.database.entities.ProjectEntity
+import com.pseandroid2.dailydata.model.database.entities.ProjectUserMap
 
-@Database(entities = [ProjectEntity::class], version = 1)
+@Database(entities = [ProjectEntity::class, ProjectUserMap::class], version = 1)
+@TypeConverters(
+    DateTimeConversion::class,
+    UserConversion::class,
+    TransformationConversion::class,
+    GraphTypeConversion::class,
+    UIElementTypeConversion::class
+)
 abstract class AppDataBase : RoomDatabase() {
+    abstract fun projectDataDAO(): ProjectDataDAO
 
+    fun projectCDManager(): ProjectCDManager {
+        return ProjectCDManager.getInstance()
+    }
+
+    fun graphCDManager(): GraphCDManager {
+        return GraphCDManager.getInstance()
+    }
 }
