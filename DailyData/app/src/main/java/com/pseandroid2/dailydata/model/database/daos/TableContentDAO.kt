4dd8c453a912231df82dcbc483d519cca0a46a82
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-01-04 15:17:16 +0100

    Change getrows function to only be able to observe one project

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/daos/TableContentDAO.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/daos/TableContentDAO.kt
@@ -1,45 +1,47 @@
 package com.pseandroid2.dailydata.model.database.daos
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Update
-import androidx.room.Delete
-import androidx.room.Query
+import androidx.room.*
 import com.pseandroid2.dailydata.model.Row
 import com.pseandroid2.dailydata.model.database.entities.RowEntity
+import com.pseandroid2.dailydata.model.table.ArrayListRow
+import com.pseandroid2.dailydata.model.toRowEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 @Dao
 abstract class TableContentDAO {
-    fun getRowsByIds(vararg ids: Int): Flow<List<Row>> {
-        return getRowEntitiesByIds(ids = ids).map {
+    fun getRowsById(id: Int): Flow<List<Row>> {
+        return getRowEntitiesById(id).map {
             val list: MutableList<Row> = ArrayList()
             for (entity: RowEntity in it) {
-                list.add()
+                list.add(ArrayListRow.createFromEntity(entity))
             }
             list.toList()
         }
     }
 
     fun insertRow(row: Row, projectId: Int) {
-        //TODO Convert Row to a RowEntity
+        insertRowEntity(row.toRowEntity(projectId))
     }
 
     fun deleteRows(projectId: Int, vararg rows: Row) {
-        //TODO Convert Row to a RowEntity
+        for (row: Row in rows) {
+            deleteRowEntities(row.toRowEntity(projectId))
+        }
     }
 
     fun changeRows(projectId: Int, vararg rows: Row) {
-        //TODO Convert Row to a RowEntity
+        for (row: Row in rows) {
+            changeRowEntities(row.toRowEntity(projectId))
+        }
     }
 
     /*========================SHOULD ONLY BE CALLED FROM INSIDE THE MODEL=========================*/
     @Insert
     abstract fun insertRowEntity(row: RowEntity)
 
-    @Query("SELECT * FROM `row` WHERE projectId IN (:ids)")
-    abstract fun getRowEntitiesByIds(vararg ids: Int): Flow<List<RowEntity>>
+    @Query("SELECT * FROM `row` WHERE projectId = :id")
+    abstract fun getRowEntitiesById(id: Int): Flow<List<RowEntity>>
 
     @Delete
     abstract fun deleteRowEntities(vararg rows: RowEntity)
