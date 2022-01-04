e02761e2f10bbc2eed1022fb58cf2e7fe7e9bb11
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-01-04 15:15:06 +0100

    Move table stuff in own package and create a Row implementation

---
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/table/ArrayListRow.kt
@@ -0,0 +1,43 @@
+package com.pseandroid2.dailydata.model.table
+
+import com.google.gson.Gson
+import com.pseandroid2.dailydata.model.Row
+import com.pseandroid2.dailydata.model.RowMetaData
+import com.pseandroid2.dailydata.model.database.entities.RowEntity
+
+class ArrayListRow(
+    private val values: List<Any>,
+    var rowMetaData: RowMetaData
+) : Row {
+
+    companion object {
+        fun createFromEntity(entity: RowEntity): ArrayListRow {
+            val metaData: RowMetaData =
+                RowMetaData(entity.createdOn, entity.publishedOnServer, entity.createdBy)
+            val values = getValuesFromJSON(entity.values)
+            return ArrayListRow(values, metaData)
+        }
+
+        private fun getValuesFromJSON(json: String): List<Any> {
+            val list = Gson().fromJson<ArrayList<Any>>(json, ArrayList::class.java)
+            //TODO make sure Integers actually become Integers and stuff
+            return list ?: listOf()
+        }
+    }
+
+    override fun getAll(): List<Any> {
+        return values
+    }
+
+    override fun getCell(col: Int): Any {
+        return values[col]
+    }
+
+    override fun getMetaData(): RowMetaData {
+        return rowMetaData
+    }
+
+    override fun getSize(): Int {
+        return values.size
+    }
+}
