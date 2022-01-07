af479f81d5893f13fd91c7790258f92418c4f21b
Arne Kuchenbecker <60854676+Altharya@users.noreply.github.com>
2022-01-07 14:18:31 +0100

    Add new Entites and DAO-getters

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/AppDataBase.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/AppDataBase.kt
@@ -4,15 +4,26 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.pseandroid2.dailydata.model.database.daos.GraphCDManager
+import com.pseandroid2.dailydata.model.database.daos.GraphDAO
+import com.pseandroid2.dailydata.model.database.daos.NotificationsDAO
 import com.pseandroid2.dailydata.model.database.daos.ProjectCDManager
 import com.pseandroid2.dailydata.model.database.daos.ProjectDataDAO
+import com.pseandroid2.dailydata.model.database.daos.TableContentDAO
+import com.pseandroid2.dailydata.model.database.daos.UIElementDAO
 import com.pseandroid2.dailydata.model.database.entities.GraphEntity
 import com.pseandroid2.dailydata.model.database.entities.ProjectEntity
 import com.pseandroid2.dailydata.model.database.entities.ProjectUserMap
 import com.pseandroid2.dailydata.model.database.entities.RowEntity
+import com.pseandroid2.dailydata.model.database.entities.UIElementMap
 
 @Database(
-    entities = [ProjectEntity::class, ProjectUserMap::class, RowEntity::class, GraphEntity::class],
+    entities = [
+        ProjectEntity::class,
+        ProjectUserMap::class,
+        RowEntity::class,
+        GraphEntity::class,
+        UIElementMap::class
+    ],
     version = 1
 )
 @TypeConverters(
@@ -25,11 +36,19 @@ import com.pseandroid2.dailydata.model.database.entities.RowEntity
 abstract class AppDataBase : RoomDatabase() {
     abstract fun projectDataDAO(): ProjectDataDAO
 
+    abstract fun uiElementDAO(): UIElementDAO
+
+    abstract fun graphDAO(): GraphDAO
+
+    abstract fun notificationsDAO(): NotificationsDAO
+
+    abstract fun tableContentDAO(): TableContentDAO
+
     fun projectCDManager(): ProjectCDManager {
-        return ProjectCDManager.getInstance()
+        return ProjectCDManager.getInstance(this)
     }
 
     fun graphCDManager(): GraphCDManager {
-        return GraphCDManager.getInstance()
+        return GraphCDManager.getInstance(this)
     }
 }
