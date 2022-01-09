10ad16586f5a8837977a7bf4cc300eafcbb5f740
kittyahy <78612604+kittyahy@users.noreply.github.com>
2022-01-09 17:53:22 +0100

    initalized The Server, added the packages, added greetings with test, (without for ServerGreetings, but this is not necessary)

---
+++ DailyDataServer/src/test/java/com/pseandroid2/dailydataserver/GreetingControllerTest.java
@@ -0,0 +1,29 @@
+package com.pseandroid2.dailydataserver;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class GreetingControllerTest {
+
+    @Autowired
+    private MockMvc mock;
+
+    @MockBean
+    private ServerGreetings serverGreetings;
+
+    @Test
+    public void testGreets() throws Exception {
+        Mockito.when(serverGreetings.greeting()).thenReturn("Hello");
+
+        mock.perform(get("/")).andExpect(jsonPath("$", Matchers.equalTo("Hello")));
+    }
+}
