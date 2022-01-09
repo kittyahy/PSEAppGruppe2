914171c4bd77ce5759b6e528617329bc8a826638
Arne Kuchenbecker <60854676+Altharya@users.noreply.github.com>
2022-01-09 13:52:31 +0100

    Add more Entities, remove CDManager getter and make class singleton

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/AppDataBase.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/AppDataBase.kt
@@ -1,6 +1,8 @@
 package com.pseandroid2.dailydata.model.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.pseandroid2.dailydata.model.database.daos.GraphCDManager
@@ -11,7 +13,13 @@ import com.pseandroid2.dailydata.model.database.daos.ProjectDataDAO
 import com.pseandroid2.dailydata.model.database.daos.TableContentDAO
 import com.pseandroid2.dailydata.model.database.daos.UIElementDAO
 import com.pseandroid2.dailydata.model.database.entities.GraphEntity
+import com.pseandroid2.dailydata.model.database.entities.GraphSettingEntity
+import com.pseandroid2.dailydata.model.database.entities.GraphTemplateEntity
+import com.pseandroid2.dailydata.model.database.entities.MissingSlotEntity
+import com.pseandroid2.dailydata.model.database.entities.NotificationEntity
 import com.pseandroid2.dailydata.model.database.entities.ProjectEntity
+import com.pseandroid2.dailydata.model.database.entities.ProjectSettingEntity
+import com.pseandroid2.dailydata.model.database.entities.ProjectTemplateEntity
 import com.pseandroid2.dailydata.model.database.entities.ProjectUserMap
 import com.pseandroid2.dailydata.model.database.entities.RowEntity
 import com.pseandroid2.dailydata.model.database.entities.UIElementMap
@@ -22,7 +30,13 @@ import com.pseandroid2.dailydata.model.database.entities.UIElementMap
         ProjectUserMap::class,
         RowEntity::class,
         GraphEntity::class,
-        UIElementMap::class
+        UIElementMap::class,
+        MissingSlotEntity::class,
+        GraphTemplateEntity::class,
+        ProjectTemplateEntity::class,
+        NotificationEntity::class,
+        ProjectSettingEntity::class,
+        GraphSettingEntity::class
     ],
     version = 1
 )
@@ -34,6 +48,24 @@ import com.pseandroid2.dailydata.model.database.entities.UIElementMap
     UIElementTypeConversion::class
 )
 abstract class AppDataBase : RoomDatabase() {
+
+    companion object {
+        private var instance: AppDataBase? = null
+
+        /**
+         * @throws NullPointerException when database creation fails
+         */
+        fun getInstance(context: Context): AppDataBase {
+            if (instance == null) {
+                synchronized(this)
+                {
+                    Room.databaseBuilder(context, AppDataBase::class.java, "app_database").build()
+                }
+            }
+            return instance!!
+        }
+    }
+
     abstract fun projectDataDAO(): ProjectDataDAO
 
     abstract fun uiElementDAO(): UIElementDAO
@@ -43,12 +75,4 @@ abstract class AppDataBase : RoomDatabase() {
     abstract fun notificationsDAO(): NotificationsDAO
 
     abstract fun tableContentDAO(): TableContentDAO
-
-    fun projectCDManager(): ProjectCDManager {
-        return ProjectCDManager.getInstance(this)
-    }
-
-    fun graphCDManager(): GraphCDManager {
-        return GraphCDManager.getInstance(this)
-    }
 }
