e6067e8fa141858a0aeb097aadeced48782c9ed4
Arne Kuchenbecker <60854676+Altharya@users.noreply.github.com>
2022-01-09 13:57:45 +0100

    Add missing primaryKeys

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/entities/MissingSlotEntity.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/entities/MissingSlotEntity.kt
@@ -3,7 +3,7 @@ package com.pseandroid2.dailydata.model.database.entities
 import androidx.room.Entity
 import java.time.LocalDateTime
 
-@Entity(tableName = "missingSlot")
+@Entity(tableName = "missingSlot", primaryKeys = ["projectId", "from", "until"])
 data class MissingSlotEntity(
     val projectId: Int,
     val from: LocalDateTime,
