6e0db97e165a2358a68341f80437815b9d9d243f
Arne Kuchenbecker <60854676+Altharya@users.noreply.github.com>
2022-01-09 13:50:01 +0100

    Initial commit

---
+++ DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/model/ProjectDatabaseTester.kt
@@ -0,0 +1,91 @@
+package com.pseandroid2.dailydata.model
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.pseandroid2.dailydata.model.database.AppDataBase
+import com.pseandroid2.dailydata.model.database.daos.ProjectDataDAO
+import com.pseandroid2.dailydata.model.database.entities.ProjectEntity
+import com.pseandroid2.dailydata.model.database.entities.ProjectSkeletonEntity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class ProjectDatabaseTester {
+    private lateinit var db: AppDataBase
+    private lateinit var projectDAO: ProjectDataDAO
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDataBase::class.java).build()
+        projectDAO = db.projectDataDAO()
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testProjectInsertion() = runTest {
+
+        val list = async { projectDAO.getAllProjectData().first { return@first it.isNotEmpty() } }
+
+        projectDAO.insertProjectEntity(
+            ProjectEntity(
+                1,
+                ProjectSkeletonEntity("Test", "", "", ""),
+                SimpleUser("", ""),
+                1
+            )
+        )
+
+        val deferredList = list.await()
+
+        val lastId = deferredList[0].id
+        val lastName = deferredList[0].name
+
+        assertEquals(1, lastId)
+        assertEquals("Test", lastName)
+
+
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testMultipleInsertions() = runTest {
+        val noProjects = 5
+
+        val list = async {
+            projectDAO.getAllProjectData().drop(noProjects - 1).first()
+        }
+
+        val user = SimpleUser("", "")
+
+        for (i in 0 until noProjects) {
+            projectDAO.insertProjectEntity(
+                ProjectEntity(
+                    i,
+                    ProjectSkeletonEntity("Test$i", "", "", ""),
+                    user,
+                    i.toLong()
+                )
+            )
+        }
+
+        val deferredList = list.await()
+
+        assertEquals(noProjects, deferredList.size)
+        for (i in 0 until noProjects) {
+            assertEquals(i, deferredList[i].id)
+            assertEquals("Test$i", deferredList[i].name)
+            assertEquals(i.toLong() + 1, deferredList[i].onlineId)
+        }
+
+    }
+
+
+}
+
