7a302b4d4be64ecb22163c3ebf4cac30cb9b034c
MPatchMob <uhlnb@student.kit.edu>
2022-01-16 08:51:03 +0100

    Todo in ProjectHandler erstellt

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelInterface/ProjectHandler.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelInterface/ProjectHandler.kt
@@ -1,4 +1,7 @@
 package com.pseandroid2.dailydata.repository.viewModelInterface
 
+import com.pseandroid2.dailydata.repository.viewModelInterface.communicationClasses.ProjectPreview
+
 class ProjectHandler {
+    val projectPreviews: Collection<ProjectPreview> = TODO()
 }
