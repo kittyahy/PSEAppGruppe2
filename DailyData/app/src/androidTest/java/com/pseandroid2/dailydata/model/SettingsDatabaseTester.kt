52bc5348a0903b5f5f7bd2190ff0cde416c7b899
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-01-16 19:12:51 +0100

    Finish CheckInsertion Test

--- DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/model/SettingsDatabaseTester.kt
+++ DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/model/SettingsDatabaseTester.kt
@@ -5,9 +5,11 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.pseandroid2.dailydata.model.database.AppDataBase
 import com.pseandroid2.dailydata.model.database.daos.SettingsDAO
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -22,19 +24,24 @@ class SettingsDatabaseTester {
         settingsDAO = db.settingsDAO()
     }
 
+    @ExperimentalCoroutinesApi
     @Test
     fun checkInsertion() = runTest {
         val map = mapOf<String, String>(Pair("A", "1"), Pair("B", "2"))
         val settings = MapSettings(map)
         val result = async {
             settingsDAO.getProjectSettings(1).first {
+                @Suppress("Deprecation")
                 it.getAllSettings().size == 2
             }
         }
         for (setting in settings) {
-            settingsDAO.createProjectSetting(1, setting.key, setting.value)
+            settingsDAO.createProjectSetting(1, setting.first, setting.second)
         }
 
+        val awaitedResult = result.await()
 
+        assertEquals(map["A"], awaitedResult["A"])
+        assertEquals(map["B"], awaitedResult["B"])
     }
 }
