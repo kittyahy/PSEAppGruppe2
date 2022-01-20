33f2a66a2a1550d5e680c276bd63013cde1bf46a
MPatchMob <uhlnb@student.kit.edu>
2022-01-20 10:25:26 +0100

    Voll funktionstüchtige Version von ProjectPreviewFlow und Test

--- DailyData/app/src/test/java/com/pseandroid2/dailydata/repository/viewModelInterface/communicationClasses/flows/ProjectPreviewFlowTest.kt
+++ DailyData/app/src/test/java/com/pseandroid2/dailydata/repository/viewModelInterface/communicationClasses/flows/ProjectPreviewFlowTest.kt
@@ -4,6 +4,7 @@ import com.pseandroid2.dailydata.model.database.entities.ProjectData
 import junit.framework.TestCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
@@ -32,44 +33,20 @@ class ProjectPreviewFlowTest : TestCase() {
         val projectDataFlow = MutableSharedFlow<List<ProjectData>>()
         val projectPreviewFlow = ProjectPreviewFlow(projectDataFlow)
         val flow = projectPreviewFlow.getProjectPreviewFlow()
-        val  collectorJob = launch {
-            flow.collect{ it -> println(it.size)}
+        val emissionsCount = 5
+        launch {
+            flow.collect {
+                assertEquals(it.last().id, it.size.toLong())
+                if (it.size >= emissionsCount) {
+                    this.cancel()
+                }
+            }
         }
         launch {
-            for(i in 1..5){
+            for(i in 1..emissionsCount){
                 list.add(ProjectData(i, "TestName", "TestDescription", i.toLong(), "TestWallpaper" ))
                 projectDataFlow.emit(list)
             }
-            collectorJob.cancel()
         }
     }
-
-    /*@Module
-    @InstallIn(DailyDataApp::class)
-    object AppDataBaseTestModule {
-        @Singleton
-        @Provides
-        fun bindAnalyticsService(): ProjectDataDAO {
-            val projectDataDAO = mockk<ProjectDataDAO>()
-            val flow = MutableSharedFlow<List<ProjectData>>()
-
-            launch {
-                var i: Int = 0
-                val list = ArrayList<ProjectData>()
-                while (true) {
-                    i++
-                    val projectData: ProjectData =
-                        ProjectData(i, "testName", "testDescription", 0, "testWallpaper")
-                    delay(10000)
-                    list.add(projectData)
-                    flow.emit(list)
-                }
-            }
-
-            every { projectDataDAO.getAllProjectData() } returns flow
-
-            return projectDataDAO
-        }
-    }*/
-
 }
