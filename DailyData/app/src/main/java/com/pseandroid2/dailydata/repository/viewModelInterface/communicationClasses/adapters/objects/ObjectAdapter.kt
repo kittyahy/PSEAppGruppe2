7b80bc59aa978d43e0dff031c3d2911bdcde2aa7
MPatchMob <uhlnb@student.kit.edu>
2022-01-21 14:23:52 +0100

    Entfernen der Object Adapter

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelInterface/communicationClasses/adapters/objects/ObjectAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.pseandroid2.dailydata.repository.viewModelInterface.communicationClasses.adapters.objects
-
-import com.pseandroid2.dailydata.model.database.AppDataBase
-
-abstract class ObjectAdapter <T> {
-    abstract val appDataBase: AppDataBase
-    abstract fun get(): T
-    abstract fun store(obj: T)
-}
