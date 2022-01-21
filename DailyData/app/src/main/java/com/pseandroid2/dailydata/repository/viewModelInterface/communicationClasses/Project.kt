2033be26a3933e0e557b414be35839a8a0fade0e
MPatchMob <uhlnb@student.kit.edu>
2022-01-21 16:42:46 +0100

    update Funktionen für Projekt

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelInterface/communicationClasses/Project.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelInterface/communicationClasses/Project.kt
@@ -1,6 +1,8 @@
 package com.pseandroid2.dailydata.repository.viewModelInterface.communicationClasses
 
 import android.graphics.drawable.Drawable
+import com.pseandroid2.dailydata.model.database.entities.GraphEntity
+import com.pseandroid2.dailydata.model.database.entities.ProjectData
 
 class Project(
     override val id: Long = 0,
@@ -15,4 +17,18 @@ class Project(
     val notifications: List<Notification> = ArrayList<Notification>(),
     val graphs: List<Graph> = ArrayList<Graph>(),
     val members: List<Member> = ArrayList<Member>()
-): Identifiable
+): Identifiable {
+    fun update(graphEntity: GraphEntity){
+        TODO("not yet implemented")
+    }
+    fun update(notification: com.pseandroid2.dailydata.model.notifications.Notification){
+        TODO("not yet implemented")
+    }
+    fun update(projectData: ProjectData){
+        TODO("not yet implemented")
+    }
+    //Todo nothing mit typ für settings ersetzen
+    fun update(settings: Nothing){
+        TODO("not yet implemented")
+    }
+}
