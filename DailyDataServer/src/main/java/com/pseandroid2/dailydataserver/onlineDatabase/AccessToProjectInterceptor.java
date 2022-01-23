0390662fcf11e4908a5fe8629198c851fd1897a4
kittyahy <78612604+kittyahy@users.noreply.github.com>
2022-01-23 10:41:32 +0100

    rumpf für alle Interceptoren gemacht

--- DailyDataServer/src/main/java/com/pseandroid2/dailydataserver/onlineDatabase/AccessToProjectInterceptor.java
+++ DailyDataServer/src/main/java/com/pseandroid2/dailydataserver/onlineDatabase/AccessToProjectInterceptor.java
@@ -21,9 +21,11 @@ package com.pseandroid2.dailydataserver.onlineDatabase;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * #TODO javadoc,Test, implemetierung
@@ -41,8 +43,13 @@ public class AccessToProjectInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+       String user = (String) request.getAttribute("user");
+        Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long projectId =(long) pathVariables.get("id");
+
+        //überprüfung
         return HandlerInterceptor.super.preHandle(request, response, handler);
 
-        //has to check, if the user can access the project.
+
     }
 }
