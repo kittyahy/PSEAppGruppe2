f12d47d12f157fbaa8d251bdeffc4f97086b22d7
Merlin Opp <94002767+MerlinTheOneAndOnlyWizard@users.noreply.github.com>
2022-01-23 20:43:00 +0100

    gradle update damit Tests endlich wieder funktionieren

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/remoteDataSource/serverConnection/serverParameter/AddPostParameter.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/remoteDataSource/serverConnection/serverParameter/AddPostParameter.kt
@@ -1,3 +1,9 @@
 package com.pseandroid2.dailydata.remoteDataSource.serverConnection.serverParameter
 
-data class AddPostParameter(val token: String, val postRreview: String, val projectTemplate: String, val graphTemplate: Collection<String>)
+/**
+ * @param token: The authentication token
+ * @param postPreview: The preview of the post as JSON
+ * @param projectTemplate: The project template as JSON
+ * @param graphTemplate: The graph templates as Collection of JSONs
+ */
+data class AddPostParameter(val token: String, val postPreview: String, val projectTemplate: String, val graphTemplate: Collection<String>)
--- DailyData/app/src/main/java/com/pseandroid2/dailydata/remoteDataSource/serverConnection/serverReturns/TemplateDetail.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/remoteDataSource/serverConnection/serverReturns/TemplateDetail.kt
@@ -8,4 +8,6 @@ import com.pseandroid2.dailydata.model.ProjectTemplate
  * @param detail: The template as a JSON
  * @param isProjectTemplate: If true, than a projectTemplate is described. If false, than a graphTemplate is described
  */
-data class TemplateDetail(val id: Int = 0, val detail: String = "", val isProjectTemplate: Boolean = false)
+data class TemplateDetail(val id: Int = 0,
+                          val detail: String = "",
+                          val isProjectTemplate: Boolean = false)
