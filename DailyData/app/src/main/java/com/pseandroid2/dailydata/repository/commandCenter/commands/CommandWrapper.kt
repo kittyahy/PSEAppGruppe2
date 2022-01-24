27cbb321153f9dc2481739e8d36468dd66836568
MPatchMob <uhlnb@student.kit.edu>
2022-01-24 11:16:10 +0100

    Implementierung CommandWrapper

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/CommandWrapper.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/CommandWrapper.kt
@@ -1,29 +1,37 @@
 package com.pseandroid2.dailydata.repository.commandCenter.commands
 
-class CommandWrapper(
-    val commandCanonicalName: String,
-    val commandJson: String
-) {
-    constructor(command: ProjectCommand) : this(canonicalNameFrom(command), jsonFrom(command))
+import com.google.gson.Gson
+
+class CommandWrapper(command: ProjectCommand) {
+
+    private val commandCanonicalName: String = canonicalNameFrom(command)
+    private val commandJson: String = jsonFrom(command)
 
     companion object {
         @JvmStatic
         private fun canonicalNameFrom(command: ProjectCommand): String {
-            return TODO()
+            return command.javaClass.canonicalName!!
         }
 
         @JvmStatic
         private fun jsonFrom(command: ProjectCommand): String {
-            return TODO()
+            return Gson().toJson(command)
+        }
+        @JvmStatic
+        fun fromJson(commandWrapperJsonString: String) : ProjectCommand {
+            val wrapper : CommandWrapper = Gson().fromJson(commandWrapperJsonString, CommandWrapper::class.java)
+            return wrapper.unwrap()
         }
     }
 
     fun toJson(): String {
-        return TODO()
+        return Gson().toJson(this)
     }
 
-    fun unwrap(): ProjectCommand {
-        return TODO()
+    private fun unwrap(): ProjectCommand {
+        //Cast cannot fail, because constructor ensures that commandCanonicalName is the canonical name of a child of ProjectCommand
+        val type: Class<out ProjectCommand> = Class.forName(commandCanonicalName) as Class<out ProjectCommand>
+        return Gson().fromJson(commandJson, type)
     }
 
 }
