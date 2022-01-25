d3b18a6ba822a07788e255bd78f0295137e87742
MPatchMob <uhlnb@student.kit.edu>
2022-01-25 15:21:14 +0100

    Arbeit an CreateProject

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/CreateProject.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/CreateProject.kt
@@ -1,5 +1,11 @@
 package com.pseandroid2.dailydata.repository.commandCenter.commands
 
 class CreateProject (projectID: Int, commandByUser: String) : ProjectCommand(projectID, commandByUser = commandByUser, isProjectAdmin = true) {
-    
+    override fun execute() {
+
+        super.execute()
+    }
+
+    override fun publish() {
+    }
 }
