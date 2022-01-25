696aee9f6a929a91cda4a49efcd6c883c5e703a5
Arne Kuchenbecker <60854676+Altharya@users.noreply.github.com>
2022-01-25 12:26:10 +0100

    Initial commit

---
+++ DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/MPChartsTester.kt
@@ -0,0 +1,16 @@
+package com.pseandroid2.dailydata
+
+/*
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.github.MikePhil.
+import org.junit.Test
+
+class MPChartsTester {
+
+    @Test
+    fun test() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val chart: LineChart
+    }
+}*/
