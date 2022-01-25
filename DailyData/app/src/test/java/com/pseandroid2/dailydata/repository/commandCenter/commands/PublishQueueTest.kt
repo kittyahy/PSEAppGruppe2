2d14d92638fde73ef62171db9a6d8a25fd2e9483
MPatchMob <uhlnb@student.kit.edu>
2022-01-25 10:54:03 +0100

    Publish Queue test Wort ausgetauscht

--- DailyData/app/src/test/java/com/pseandroid2/dailydata/repository/commandCenter/commands/PublishQueueTest.kt
+++ DailyData/app/src/test/java/com/pseandroid2/dailydata/repository/commandCenter/commands/PublishQueueTest.kt
@@ -1,19 +1,12 @@
 package com.pseandroid2.dailydata.repository.commandCenter.commands
 
-import com.pseandroid2.dailydata.model.database.entities.ProjectData
 import com.pseandroid2.dailydata.remoteDataSource.RemoteDataSourceAPI
-import com.pseandroid2.dailydata.repository.viewModelAPI.communicationClasses.adapters.flows.ProjectPreviewFlow
 import io.mockk.every
 import io.mockk.mockk
 import junit.framework.TestCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.flow.collect
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -32,24 +25,30 @@ class PublishQueueTest : TestCase() {
     @ExperimentalCoroutinesApi
     @Test
     fun testGetProjectPreviewFlow() = runTest(){
-        val myCommand1 = QueueTestCommand()
+        val expected = "Rindfleischettikettierungsüberwachungsaufgabenübertragungsgesetz"
+        val myCommandList = ArrayList<QueueTestCommand>()
+        for (i in 1..expected.length) {
+            myCommandList.add(QueueTestCommand(expected.substring(i-1,i)))
+        }
         val remoteDataSourceAPI = mockk<RemoteDataSourceAPI>()
         every {remoteDataSourceAPI.connectionToServerPossible()} returns true
         publishQueue = PublishQueue(remoteDataSourceAPI)
-        publishQueue.add(myCommand1)
-        val oldContent = myCommand1.content
-        while(myCommand1.content == 0) {
+        for(myCommand in myCommandList) {
+            publishQueue.add(myCommand)
         }
-        assertEquals(1, myCommand1.content)
+        val oldContent = QueueTestCommand.content
+        while(QueueTestCommand.content != expected) {
+        }
+        assertEquals(expected, QueueTestCommand.content)
+        println(QueueTestCommand.content)
     }
 }
-class QueueTestCommand() : ProjectCommand(3) {
-    var content: Int = 0
+class QueueTestCommand(private val concat:String) : ProjectCommand(3) {
+    companion object {var content: String = ""}
     override fun individualExecution() {
-        TODO("Not yet implemented")
     }
 
     override fun publish() {
-        content += 1
+        content += concat
     }
 }
