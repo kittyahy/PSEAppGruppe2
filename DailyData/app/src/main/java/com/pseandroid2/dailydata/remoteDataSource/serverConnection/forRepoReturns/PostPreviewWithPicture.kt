8c52778c8c6b793e88fc5a70f7c7dd6f80fd3232
Merlin Opp <94002767+MerlinTheOneAndOnlyWizard@users.noreply.github.com>
2022-02-03 15:46:59 +0100

    JavaDocs added

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/remoteDataSource/serverConnection/forRepoReturns/PostPreviewWithPicture.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/remoteDataSource/serverConnection/forRepoReturns/PostPreviewWithPicture.kt
@@ -2,6 +2,13 @@ package com.pseandroid2.dailydata.remoteDataSource.serverConnection.forRepoRetur
 
 import android.graphics.Bitmap
 
+/**
+ * A dataclass which will be passed to the repository. It stores a single post preview
+ *
+ * @param id:               The id of the post
+ * @param preview:          The PostPreview as a JSON
+ * @param previewPicture:   The preview image of the post
+ */
 data class PostPreviewWithPicture(val id: Int = 0,
                                   val preview: String = "post preview",
                                   val previewPicture: Bitmap
