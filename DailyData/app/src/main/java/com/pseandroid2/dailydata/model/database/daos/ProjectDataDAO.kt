4934f126ea676a7a945f7e029f5f6d9b983fc584
Arne Kuchenbecker <60854676+Altharya@users.noreply.github.com>
2022-02-03 22:00:13 +0100

    Add functions to translate between onlineId and id

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/daos/ProjectDataDAO.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/model/database/daos/ProjectDataDAO.kt
@@ -49,6 +49,12 @@ abstract class ProjectDataDAO {
     @Query("SELECT isOnline FROM project WHERE id = :id")
     abstract fun isOnline(id: Int): Flow<Boolean>
 
+    @Query("SELECT onlineId FROM project WHERE id = :id")
+    abstract fun getOnlineId(id: Int): Long
+
+    @Query("SELECT id FROM project WHERE onlineId = :onlineId LIMIT 1")
+    abstract fun getIdForOnlineId(onlineId: Long): Int
+
     @Query("UPDATE project SET name = :name WHERE id = :id")
     abstract suspend fun setName(id: Int, name: String)
 
