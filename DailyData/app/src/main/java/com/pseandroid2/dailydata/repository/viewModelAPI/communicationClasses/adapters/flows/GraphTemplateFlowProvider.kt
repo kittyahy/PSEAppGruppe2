0897b9633615468ad3dd66f1c21d4a4cd4bb2508
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2022-02-03 11:54:12 +0100

    Rename as this is a FlowProvider

---
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelAPI/communicationClasses/adapters/flows/GraphTemplateFlowProvider.kt
@@ -0,0 +1,41 @@
+package com.pseandroid2.dailydata.repository.viewModelAPI.communicationClasses.adapters.flows
+
+import com.pseandroid2.dailydata.model.database.AppDataBase
+import com.pseandroid2.dailydata.model.database.daos.GraphCDManager.Companion.TEMPLATE_SETTINGS_PROJ_ID
+import com.pseandroid2.dailydata.model.graph.GraphTemplate
+import com.pseandroid2.dailydata.model.graph.GraphType
+import com.pseandroid2.dailydata.model.graph.SimpleGraphTemplate
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.distinctUntilChanged
+
+
+class GraphTemplateFlow(private val templateId: Int, private val db: AppDataBase) :
+    FlowProvider<GraphTemplate?>() {
+    private var template: GraphTemplate = SimpleGraphTemplate(type = GraphType.PIE_CHART)
+    override suspend fun initialize() = coroutineScope {
+        //Observe GraphTemplateData
+        launch(Dispatchers.IO) {
+            db.templateDAO().getGraphTemplateData(templateId).distinctUntilChanged()
+                .collect { templateData ->
+                    if (templateData.size != 1) {
+                        //Multiple or no Templates with this Id
+                        mutableFlow.emit(null)
+                    } else {
+                        val data = templateData[0]
+                        template = SimpleGraphTemplate(data)
+                    }
+                }
+        }
+        //Observe Template Settings
+        launch(Dispatchers.IO) {
+            db.settingsDAO().getGraphSettings(TEMPLATE_SETTINGS_PROJ_ID, templateId)
+                .distinctUntilChanged().collect { settings ->
+                    template.customizing = settings
+                }
+        }
+        Unit
+    }
+
+}
