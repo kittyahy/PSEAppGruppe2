099fd15d82608b9050061951e7316818da4eb42c
MPatchMob <uhlnb@student.kit.edu>
2022-02-10 08:47:33 +0100

    Serverhandler verwendet Flow Map

--- DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelAPI/ServerHandler.kt
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/viewModelAPI/ServerHandler.kt
@@ -32,10 +32,27 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.runBlocking
 
 class ServerHandler(private val appDataBase: AppDataBase, private val api: RemoteDataSourceAPI) {
+    private val amILoggedInKey = "amILoggedIn"
+    private val defaultKey = "default"
+    private val loginKey = "login"
+    private val supportedCommands = mapOf<String, Boolean>(
+        Pair(defaultKey, true),
+        Pair(amILoggedInKey, api.getUserName() != ""),
+        Pair(loginKey, api.getUserName() == "")
+    )
+    private val isPossibleMap = mutableMapOf<String, MutableSharedFlow<Boolean>>()
+
+    init {
+        supportedCommands.forEach {
+            val flow = MutableSharedFlow<Boolean>(1)
+            runBlocking { flow.emit(it.value) }//Todo runBlocking weg
+            isPossibleMap[it.key] = flow
+        }
+    }
+
     suspend fun getPostPreviews(): Collection<PostPreview> = coroutineScope {
         val arrayList = ArrayList<PostPreview>()
         val postPreviews = async(Dispatchers.IO) { api.getPostPreviews() }
@@ -54,10 +71,8 @@ class ServerHandler(private val appDataBase: AppDataBase, private val api: Remot
         TODO("getProjectTemplateById")
     }
 
-    fun amILoggedIn() = flow {
-        val string = api.getUserName()
-        emit(string != "")
-        kotlinx.coroutines.delay(500)
+    fun amILoggedIn(): Flow<Boolean> {
+        return isPossibleMap[amILoggedInKey]!!
     }
 
     /**
@@ -67,19 +82,13 @@ class ServerHandler(private val appDataBase: AppDataBase, private val api: Remot
      *      users should not be able to call manipulation().
      */
     fun loginIsPossible(): Flow<Boolean> {
-        //Todo replace with valid proof
-        /*val flow = MutableSharedFlow<Boolean>()
-        runBlocking {
-            flow.emit(true)
-        }*/
-        //TODO Arne/Anton should this really only ever return true? Also, SharedFlows are Hot Flows,
-        //i.e. they broadcast emissions to whoever is currently listening, but will not send their
-        //latest emission to new subscribers
-        return flow { emit(true) }
+        return isPossibleMap[loginKey]!!
     }
 
-    suspend fun login(email: String, password: String) { //Todo erweiterbarkeit
-        api.signInUser(email, password, SignInTypes.EMAIL)
+    suspend fun login(email: String, password: String) {
+        isPossibleMap[amILoggedInKey]!!.emit(true)
+        isPossibleMap[loginKey]!!.emit(false)
+        api.signInUser(email, password, SignInTypes.EMAIL) //Todo erweiterbarkeit
     }
 
     /**
@@ -90,11 +99,7 @@ class ServerHandler(private val appDataBase: AppDataBase, private val api: Remot
      */
     fun signUpIsPossible(): Flow<Boolean> {
         //Todo replace with valid proof
-        val flow = MutableSharedFlow<Boolean>()
-        runBlocking {
-            flow.emit(true)
-        }
-        return flow
+        return isPossibleMap[defaultKey]!!
     }
 
     suspend fun signUp(email: String, password: String) { //Todo erweiterbarkeit
@@ -109,11 +114,7 @@ class ServerHandler(private val appDataBase: AppDataBase, private val api: Remot
      */
     fun downloadProjectTemplateIsPossible(): Flow<Boolean> {
         //Todo replace with valid proof
-        val flow = MutableSharedFlow<Boolean>()
-        runBlocking {
-            flow.emit(true)
-        }
-        return flow
+        return isPossibleMap[defaultKey]!!
     }
 
     fun downloadProjectTemplate(id: Int) {
@@ -128,11 +129,7 @@ class ServerHandler(private val appDataBase: AppDataBase, private val api: Remot
      */
     fun downloadGraphTemplateIsPossible(): Flow<Boolean> {
         //Todo replace with valid proof
-        val flow = MutableSharedFlow<Boolean>()
-        runBlocking {
-            flow.emit(true)
-        }
-        return flow
+        return isPossibleMap[defaultKey]!!
     }
 
     fun downloadGraphTemplate(projectId: Int, graphId: Int) {
