6975a34c82c85e27147776afa257d6d7a4991b8c
MPatchMob <uhlnb@student.kit.edu>
2022-02-14 18:00:28 +0100

    Impl ohne Anbindung ProjectCommand DeleteGraph

---
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/DeleteGraph.kt
@@ -0,0 +1,24 @@
+package com.pseandroid2.dailydata.repository.commandCenter.commands
+
+import com.pseandroid2.dailydata.model.graph.Graph
+import com.pseandroid2.dailydata.repository.RepositoryViewModelAPI
+import com.pseandroid2.dailydata.repository.commandCenter.PublishQueue
+import com.pseandroid2.dailydata.repository.viewModelAPI.communicationClasses.Project
+
+class DeleteGraph(projectID: Int, val graph: Graph<*, *>) : ProjectCommand(projectID = projectID) {
+    companion object {
+        fun isPossible(project: Project): Boolean {
+            return ProjectCommand.isPossible(project)
+        }
+    }
+
+    override val publishable: Boolean = false
+
+    override suspend fun execute(
+        repositoryViewModelAPI: RepositoryViewModelAPI,
+        publishQueue: PublishQueue
+    ) {
+        repositoryViewModelAPI.appDataBase.graphCDManager().deleteGraph(projectID!!, graph.id)
+        super.execute(repositoryViewModelAPI, publishQueue)
+    }
+}
