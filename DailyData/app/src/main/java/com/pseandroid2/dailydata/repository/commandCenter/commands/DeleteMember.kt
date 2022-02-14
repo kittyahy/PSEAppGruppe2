32d27342a758bcd4c5f01e480a84a80e785cf30f
MPatchMob <uhlnb@student.kit.edu>
2022-02-14 18:03:12 +0100

    Impl ohne Anbindung ProjectCommand DeleteMember

---
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/DeleteMember.kt
@@ -0,0 +1,25 @@
+package com.pseandroid2.dailydata.repository.commandCenter.commands
+
+import com.pseandroid2.dailydata.model.users.User
+import com.pseandroid2.dailydata.repository.RepositoryViewModelAPI
+import com.pseandroid2.dailydata.repository.commandCenter.PublishQueue
+import com.pseandroid2.dailydata.repository.viewModelAPI.communicationClasses.Project
+
+class DeleteMember(projectID: Int, private val user: User) :
+    ProjectCommand(projectID = projectID) {
+    companion object {
+        fun isPossible(project: Project): Boolean {
+            return project.members.size > 2 && project.isOnlineProject
+        }
+    }
+
+    override val publishable: Boolean = true
+    override suspend fun execute(
+        repositoryViewModelAPI: RepositoryViewModelAPI,
+        publishQueue: PublishQueue
+    ) {
+        repositoryViewModelAPI.appDataBase.projectDataDAO()
+            .removeUsers(projectID!!, user)
+        super.execute(repositoryViewModelAPI, publishQueue)
+    }
+}
