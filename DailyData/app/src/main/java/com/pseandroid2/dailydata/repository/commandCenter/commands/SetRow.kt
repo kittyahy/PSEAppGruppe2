2564ff87c214ceb07fa391982ebcc425f017b791
MPatchMob <uhlnb@student.kit.edu>
2022-02-15 15:36:40 +0100

    Impl ohne Anbindung ProjectCommand SetRow

---
+++ DailyData/app/src/main/java/com/pseandroid2/dailydata/repository/commandCenter/commands/SetRow.kt
@@ -0,0 +1,37 @@
+package com.pseandroid2.dailydata.repository.commandCenter.commands
+
+import com.pseandroid2.dailydata.model.table.Row
+import com.pseandroid2.dailydata.repository.RepositoryViewModelAPI
+import com.pseandroid2.dailydata.repository.commandCenter.PublishQueue
+import com.pseandroid2.dailydata.repository.viewModelAPI.communicationClasses.Project
+
+class SetRow(projectID: Int, private val row: Row) :
+    ProjectCommand(projectID = projectID) {
+    companion object {
+        fun isPossible(project: Project): Boolean {
+            return ProjectCommand.isPossible(project)
+        }
+
+        const val issuerNeedsAdminRights: Boolean = false
+
+        const val publishable: Boolean = true
+    }
+
+    override suspend fun execute(
+        repositoryViewModelAPI: RepositoryViewModelAPI,
+        publishQueue: PublishQueue
+    ) {
+        @Suppress("DEPRECATION")
+        repositoryViewModelAPI.appDataBase.tableContentDAO()
+            .changeRows(projectID!!, row)
+        super.execute(repositoryViewModelAPI, publishQueue)
+    }
+
+    override fun publish(
+        repositoryViewModelAPI: RepositoryViewModelAPI,
+        publishQueue: PublishQueue
+    ): Boolean {
+        return super.publish(repositoryViewModelAPI, publishQueue) && publishable
+    }
+
+}
