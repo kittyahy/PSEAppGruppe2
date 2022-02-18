d4f2425cdebc99f1c1c51ce0269d8b2998d9ab9b
Merlin Opp <94002767+MerlinTheOneAndOnlyWizard@users.noreply.github.com>
2022-02-18 21:22:54 +0100

    Test 7.1.33 verbessert

--- DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/globaltests/GT7133.kt
+++ DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/globaltests/GT7133.kt
@@ -1,17 +1,24 @@
 package com.pseandroid2.dailydata.globaltests
 
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.text.AnnotatedString
 import com.pseandroid2.dailydata.Main
 import com.pseandroid2.dailydata.MainActivity
+import com.pseandroid2.dailydata.util.ui.UiEvent
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
+import java.lang.AssertionError
 
 class GT7133 {
     @get:Rule
@@ -39,10 +46,14 @@ class GT7133 {
         runBlocking { delay(1000) }
         composeRule.onNodeWithText("Settings").performClick()
         runBlocking { delay(1000) }
-        composeRule.onNodeWithText("Create Link").performClick()
-        runBlocking { delay(500) }
-        //
+        TODO("Implementiere die Funktionalität: Offlineprojekt zu Onlinprojekt wechseln")
         composeRule.onNodeWithText("Create Online Project").performClick()
-
+        runBlocking { delay(500) }
+        composeRule.onNodeWithText("Create Link").performClick()
+        runBlocking { delay(1000) }
+        val clipboardManager = composeRule.activity.baseContext.getSystemService(Context.CLIPBOARD_SERVICE) as androidx.compose.ui.platform.ClipboardManager
+        val clipboard = clipboardManager.getText() // gets the stored Link from Clipboard
+        Assert.assertNotEquals(clipboard, null)
+        Assert.assertTrue(clipboard!!.startsWith("https://dailydata.page.link/?link=https://www.dailydata.com/?projectid%3D"))
     }
 }
