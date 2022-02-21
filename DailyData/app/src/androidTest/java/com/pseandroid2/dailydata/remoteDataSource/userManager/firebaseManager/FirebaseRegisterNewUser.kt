f8cac236d59ea0e99271c5fd8aca7b30c80adfd7
Merlin Opp <94002767+MerlinTheOneAndOnlyWizard@users.noreply.github.com>
2022-02-21 12:12:12 +0100

    TestUsers Object hinzugefügt

--- DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/remoteDataSource/userManager/firebaseManager/FirebaseRegisterNewUser.kt
+++ DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/remoteDataSource/userManager/firebaseManager/FirebaseRegisterNewUser.kt
@@ -4,20 +4,26 @@ import org.junit.Assert
 import org.junit.Test
 import com.pseandroid2.dailydata.remoteDataSource.userManager.FirebaseManager
 import com.pseandroid2.dailydata.remoteDataSource.userManager.FirebaseReturnOptions
+import com.pseandroid2.dailydata.remoteDataSource.userManager.TestsUsers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 
 
 class FirebaseRegisterNewUser {
-    /* // NOTE: The test works but it got removed because every time a new email and password ist needed and we don't want to have to many test firebase accounts in our firebase-project
+     // NOTE: The test works but it got removed because every time a new email and password ist needed and we don't want to have to many test firebase accounts in our firebase-project
+
+    @ExperimentalCoroutinesApi
     @Test
-    fun registerNewUser() {
+    fun registerNewUser() = runTest {
         var email = "pseFan@student.kit.edu"
         var password = "mehrSpaßAlsBeiPSEGibtsNicht"
 
         var fm = FirebaseManager(null)
 
-        fm.signOut()
 
-        //Assert.assertEquals(FirebaseReturnOptions.REGISTERED, fm.registerUserWithEmailAndPassword(email, password))
+         for (i in 3..TestsUsers.email.size-1) {
+             Assert.assertEquals(FirebaseReturnOptions.REGISTERED, fm.registerUserWithEmailAndPassword(TestsUsers.email[i], TestsUsers.password[i]))
+         }
     }
-    */
+
 }
