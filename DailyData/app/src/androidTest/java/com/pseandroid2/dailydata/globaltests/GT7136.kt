dd0253c89e2529a1894f5eb5a3011ca32c9c6f7b
Merlin Opp <94002767+MerlinTheOneAndOnlyWizard@users.noreply.github.com>
2022-02-23 21:14:15 +0100

    GT7136 hinzugefügt

--- DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/globaltests/GT7136.kt
+++ DailyData/app/src/androidTest/java/com/pseandroid2/dailydata/globaltests/GT7136.kt
@@ -30,7 +30,7 @@ import org.junit.Rule
 import org.junit.Test
 import java.util.regex.Matcher
 
-class GT7135 {
+class GT7136 {
     val rds: RemoteDataSourceAPI = RemoteDataSourceAPI(
         UserAccount(FirebaseManager(null)), ServerManager(RESTAPI(URLs.testServer_BASE_URL))
     )
@@ -38,7 +38,7 @@ class GT7135 {
     @get:Rule
     val composeRule = createAndroidComposeRule<MainActivity>()
 
-    private val projectName: String = "GT7135";
+    private val projectName: String = "GT7136";
 
 
     @ExperimentalCoroutinesApi
@@ -60,44 +60,78 @@ class GT7135 {
 
     @ExperimentalCoroutinesApi
     /**
-     * tests: "Projektadministrator, Projektmitglied", 7.1.35
+     * tests: "Neuer Projektadministrator", 7.1.36
+     * n = 1
      * This tests if the global test works in the rdsAPI
      */
     @InternalCoroutinesApi
     @Test
-    fun joinProjectRDS() = runTest {
+    fun newProjectAdminRDS() = runTest {
         var projectID: Long = -1
 
         // User N1 creates the online project
         Assert.assertTrue(rds.signInUser(TestUsers.eMail[0], TestUsers.password[0], SignInTypes.EMAIL).success)
         projectID = rds.createNewOnlineProject("projectDetails")
 
-        // User N2 joins a project
+        // User N2 joins the project
         Assert.assertTrue(rds.signInUser(TestUsers.eMail[1], TestUsers.password[1], SignInTypes.EMAIL).success)
         Assert.assertEquals(rds.joinProject(projectID), "projectDetails")
 
-        // User N1 and N2 are project member
-        val projectParticipants: MutableList<String> = rds.getProjectParticipants(projectID) as MutableList<String>
-        Assert.assertEquals(2, projectParticipants.size)
-        Assert.assertTrue(projectParticipants.remove(TestUsers.userID[0]))
-        Assert.assertTrue(projectParticipants.remove(TestUsers.userID[1]))
-        Assert.assertEquals(0, projectParticipants.size) // Which means that both project participants were N1 and N2
+        // User N1 is project admin
+        Assert.assertEquals(TestUsers.userID[0], rds.getProjectAdmin(projectID))
+
+        // Remove N1 from the project
+        Assert.assertTrue(rds.signInUser(TestUsers.eMail[0], TestUsers.password[0], SignInTypes.EMAIL).success)
+        Assert.assertTrue(rds.removeUser(TestUsers.userID[0], projectID))
+
+        // User N2 is project admin
+        Assert.assertTrue(rds.signInUser(TestUsers.eMail[1], TestUsers.password[1], SignInTypes.EMAIL).success)
+        Assert.assertEquals(TestUsers.userID[1], rds.getProjectAdmin(projectID))
+    }
+
+    @ExperimentalCoroutinesApi
+    /**
+     * tests: "Neuer Projektadministrator", 7.1.36
+     * n = 24
+     * This tests if the global test works in the rdsAPI
+     */
+    @InternalCoroutinesApi
+    @Test
+    fun newProjectAdminWithNEquals24RDS() = runTest {
+        var projectID: Long = -1
+
+        // User N1 creates the online project
+        Assert.assertTrue(rds.signInUser(TestUsers.eMail[0], TestUsers.password[0], SignInTypes.EMAIL).success)
+        projectID = rds.createNewOnlineProject("projectDetails")
+
+        // Users N2-N25 join the project
+        for (i in 1..23) {
+            Assert.assertTrue(rds.signInUser(TestUsers.eMail[i], TestUsers.password[i], SignInTypes.EMAIL).success)
+            Assert.assertEquals(rds.joinProject(projectID), "projectDetails")
+        }
 
         // User N1 is project admin
         Assert.assertEquals(TestUsers.userID[0], rds.getProjectAdmin(projectID))
+
+        // Remove N1 from the project
+        Assert.assertTrue(rds.signInUser(TestUsers.eMail[0], TestUsers.password[0], SignInTypes.EMAIL).success)
+        Assert.assertTrue(rds.removeUser(TestUsers.userID[0], projectID))
+
+        // User N2 is project admin
+        Assert.assertTrue(rds.signInUser(TestUsers.eMail[1], TestUsers.password[1], SignInTypes.EMAIL).success)
+        Assert.assertEquals(TestUsers.userID[1], rds.getProjectAdmin(projectID))
     }
 
     @Ignore("Not all for this test necessary features exists")
     /**
-     * tests: "Projektadministrator, Projektmitglied", 7.1.35
+     * tests: "Neuer Projektadministrator", 7.1.36
      */
     @InternalCoroutinesApi
     @Test
-    fun joinProject() {
+    fun newProjectAdmin() {
         TODO("Implementiere die Funktionalität: Melde dich in der App mit einem Account an")
 
         // Login User 1
-        composeRule.onNodeWithText(projectName).performClick()
         composeRule.onNodeWithTag(Routes.SERVER).performClick()
         runBlocking { delay(1000) }
         composeRule.onNodeWithText("Email").performTextInput(TestUsers.eMail[0])
@@ -108,6 +142,8 @@ class GT7135 {
         // Create Project
         composeRule.onNodeWithTag(Routes.PROJECT).performClick()
         runBlocking { delay(1000) }
+        composeRule.onNodeWithText(projectName).performClick()
+        runBlocking { delay(1000) }
         TODO("Implementiere die Funktionalität: Offlineprojekt zu Onlinprojekt wechseln")
         composeRule.onNodeWithText("Create Online Project").performClick()
         runBlocking { delay(500) }
@@ -115,8 +151,8 @@ class GT7135 {
         runBlocking { delay(1000) }
         val clipboardManager = composeRule.activity.baseContext.getSystemService(Context.CLIPBOARD_SERVICE) as androidx.compose.ui.platform.ClipboardManager
 
+
         // Login User 2
-        composeRule.onNodeWithText(projectName).performClick()
         composeRule.onNodeWithTag(Routes.SERVER).performClick()
         runBlocking { delay(1000) }
         composeRule.onNodeWithText("Email").performTextInput(TestUsers.eMail[1])
@@ -124,18 +160,48 @@ class GT7135 {
         composeRule.onNodeWithText("Login").performClick()
         runBlocking { delay(2000) }
 
+
         // Close app
         TODO("Finde eine Möglichkeit die App zu schließen und den Link in dem clipboard zu öffnen")
 
         // Join the Project
         composeRule.onNodeWithText("Join Project").performClick()
+        runBlocking { delay(1000) }
 
-        // Open the project
+        // Login User 1 and remove User 1 from the project
         composeRule.onNodeWithTag(Routes.SERVER).performClick()
+        runBlocking { delay(1000) }
+        composeRule.onNodeWithText("Email").performTextInput(TestUsers.eMail[0])
+        composeRule.onNodeWithText("Password").performTextInput(TestUsers.password[0])
+        composeRule.onNodeWithText("Login").performClick()
+        runBlocking { delay(2000) }
+        composeRule.onNodeWithTag(Routes.PROJECT).performClick()
         runBlocking { delay(1000) }
         composeRule.onNodeWithText(projectName).performClick()
         runBlocking { delay(1000) }
-        composeRule.onNodeWithText("Input").assertExists() // Means that the switch to the project screen succeeded
+        composeRule.onNodeWithText("Settings").performClick()
+        runBlocking { delay(1000) }
+        composeRule.onNodeWithText("Member").performClick()
+        runBlocking { delay(500) }
+        composeRule.onNodeWithText(TestUsers.eMail[0]).performClick() // Delete User1
+        runBlocking { delay(500) }
+        composeRule.onNodeWithText("Yes").performClick()
+        runBlocking { delay(500) }
+
+        // Login User 2
+        composeRule.onNodeWithTag(Routes.SERVER).performClick()
+        runBlocking { delay(1000) }
+        composeRule.onNodeWithText("Email").performTextInput(TestUsers.eMail[1])
+        composeRule.onNodeWithText("Password").performTextInput(TestUsers.password[1])
+        composeRule.onNodeWithText("Login").performClick()
+        runBlocking { delay(2000) }
+
+        // Open project
+        composeRule.onNodeWithTag(Routes.PROJECT).performClick()
+        runBlocking { delay(1000) }
+        composeRule.onNodeWithText(projectName).performClick()
+        runBlocking { delay(1000) }
+        composeRule.onNodeWithText("Settings").performClick()
 
         // Check who the project admin is
         TODO("Implementiere die Funktionalität: Projektadministrator anzeigen")
